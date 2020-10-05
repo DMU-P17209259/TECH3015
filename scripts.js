c35c16076f4033661c0fb2cfcd8ea65a7d0f4022
DMU-P17209259 <72386075+DMU-P17209259@users.noreply.github.com>
2020-10-05 11:34:19 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+//alert("Hello!");
+const hello = document.getElementById("hello");
+
+function greeting() {
+   alert("Hello!");
+}
+
+hello.addEventListener("click", greeting);
+
+
+var demo = document.getElementById("demo");
+function date() {
+  alert("10-10-2018");
+}
+
+demo.addEventListener("click", date);
